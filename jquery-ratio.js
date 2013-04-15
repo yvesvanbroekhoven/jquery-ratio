1c4e0c253a9de8125131d6fcd0c59a5ca7629fb8
Yves Van Broekhoven <yvesvanbroekhoven@gmail.com>
2013-04-15 14:58:45 +0200

    Initial commit

---
+++ jquery-ratio.js
@@ -0,0 +1,16 @@
+/*global jQuery:false*/
+
+/** @preserve
+ * jquery-ratio
+ *
+ * Created at: 2013-04-15 14:58:45 +0200
+ * Updated at: 2013-04-15 14:58:45 +0200
+ *
+ * Author: Yves Van Broekhoven
+ * Version: 0.0.0
+ *
+ */
+
+(function($) {
+  "use strict";
+}(jQuery));
