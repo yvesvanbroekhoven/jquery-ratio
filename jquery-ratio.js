3196c5b79e066eec2c8e4352c04aa70f253fc80e
Yves Van Broekhoven <yvesvanbroekhoven@gmail.com>
2013-04-16 11:17:17 +0200

    Removed resizing inside plugin, this can be best implemented inside the application, otherwise this will interfere with situations when you're working on a responsive site where only the mobile needs a ratio fix.

--- jquery-ratio.js
+++ jquery-ratio.js
@@ -4,10 +4,12 @@
  * jquery-ratio
  *
  * Created at: 2013-04-15 14:58:45 +0200
- * Updated at: 2013-04-15 15:12:40 +0200
+ * Updated at: 2013-04-16 11:09:03 +0200
  *
  * Author: Yves Van Broekhoven
  * Version: 0.0.1
+ *
+ * https://github.com/yvesvanbroekhoven/jquery-ratio
  *
  */
 
@@ -15,63 +17,70 @@
   "use strict";
 
   var plugin_name = "jquery_ratio",
-      defaults    = {
+      defaults = {
         new_height: null,
         new_width: null,
         ratio_height: '3',
         ratio_width: '4',
-        recalculate_on_resize: false,
         side: 'width'
       };
 
 
+  //
+  // Contructor
+  //
   function Ratio(element, options) {
     this.element = element;
     this.options = $.extend({}, defaults, options);
 
-    this.init();
+    this.set();
   }
 
-  Ratio.prototype.init = function(){
-    var _this     = this,
-        $element   = $(_this.element);
 
-    _this.set();
-
-    if ( _this.options.recalculate_on_resize ) {
-      $(window).on('resize', function(){
-        _this.destroy();
-        _this.set();
-      });
-    }
-  };
-
+  //
+  // Set element dimensions by ratio
+  //
   Ratio.prototype.set = function(){
-    var $element   = $(this.element);
+    var $element = $(this.element),
+        new_height,
+        new_width;
 
-    // calculate ratio
+    // Calculate ratio
     if ( this.options.side === 'height' ) {
-        var new_height = this.options.new_height ? parseInt(this.options.new_height, 10) : $element.height(),
-            new_width  = new_height / this.options.ratio_height * this.options.ratio_width;
+      new_height  = this.options.new_height ? parseInt(this.options.new_height, 10) : $element.height();
+      new_width   = new_height / this.options.ratio_height * this.options.ratio_width;
 
     } else {
-      var new_width  = this.options.new_width ? parseInt(this.options.new_width, 10) : $element.width(),
-          new_height = new_width / this.options.ratio_width * this.options.ratio_height;
+      new_width   = this.options.new_width ? parseInt(this.options.new_width, 10) : $element.width();
+      new_height  = new_width / this.options.ratio_width * this.options.ratio_height;
 
     }
+
     $element.css({
       height: new_height,
       width: new_width
     });
   };
 
+
+  //
+  // Destroy plugin instance & reset element dimensions
+  //
   Ratio.prototype.destroy = function(){
-    $(this.element).css({
-      height: '',
-      width: ''
-    });
+    var $element = $(this.element);
+
+    $element
+      .css({
+        height: '',
+        width: ''
+      })
+      .data(plugin_name, null);
   };
 
+
+  //
+  // Attach to jQuery
+  //
   $.fn.ratio = function(options) {
     return this.each( function(){
       if ( !$.data(this, plugin_name) ) {
